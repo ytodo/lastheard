d292d547b78a72acd4d024d5ec3742795d640308
ytodo <je3hcz@gmail.com>
2024-01-18 08:35:42 +0900

    lastheard v2.0.0 for D-STAR G1 Gateway Dashboard

--- html/monitor.php
+++ html/monitor.php
@@ -55,8 +55,8 @@
 	/* WEB を指定秒数でリフレッシュ */
 	$sec = intval($interval);
 
-	header("Refresh:$sec; url=index.php");		// index.php
-//	header("Refresh:$sec; url=monitor.php");	// monitor.php
+//	header("Refresh:$sec; url=index.php");		// index.php
+	header("Refresh:$sec; url=monitor.php");	// monitor.php
 
 
 
@@ -388,9 +388,9 @@
                 pclose($fp);
 
                 /* dstatus のバージョン情報を取得 */
-                $fp = popen("apt-cache madison rpi-dsstatus", 'r');
+                $fp = popen("apt-cache madison rpi-dstatus", 'r');
                 $line = fgets($fp);
-                $dstatus_ver = str_replace("\n", '', substr($line, 18, 5));
+                $dstatus_ver = str_replace("\n", '', substr($line, 19, 5));
                 pclose($fp);
 
                 /* decho のバージョン除法を取得 */
@@ -425,7 +425,7 @@
 	echo '<a style="font-size:12pt; color:white;" href="http://10.0.2.46:20201" target="_blank">'."rpi-xchange v".$xchange_ver.'</a><br>';
 	echo '<a style="font-size:12pt; color:white;" href="http://10.0.2.46:20202" target="_blank">'."rpi-multi_forward v".$multi_ver.'</a><br>';
 	echo '<a style="font-size:12pt; color:white;" href="http://10.0.2.46:20203" target="_blank">'."rpi-dprs v".$dprs_ver.'</a></br>';
-	echo '<a style="font-size:12pt; color:white;" href="http://10.0.2.46:20204" target="_blank">'."rpi-dstatus v".$dstatus_ver.'</a><br>';
+	echo '<a style="font-size:12pt; color:white;" href="http://10.0.2.46:20204" target="_blank">'."rpi-dstatus v".$dstatus_ver.'</a></br>';
 	echo '<a style="font-size:12pt; color:white;" href="http://10.0.2.46:20205" target="_blank">'."rpi-decho v".$echo_ver.'</a>';
 ?>
 
--- recv.h
+++ recv.h
@@ -42,6 +42,8 @@
 #include <memory.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 /* macros */
 #define N       256      // 配列標準サイズ
@@ -52,8 +54,12 @@
 unsigned int sock;
 struct	sockaddr_in addr;
 socklen_t sin_size;
+
 struct	sockaddr_in from_addr;
+socklen_t from_len = sizeof(from_addr);
+
 char	recvbuf[64];    // 受信バッファ
+char	sendbuf[16];	// 送信バッファ
 
 /* 日付表示関連 */
 time_t  timer;
@@ -62,13 +68,13 @@ char    tmstr[N] = {'\0'};
 
 /* Voice ShortData関連 */
 char	sdata[3] = {'\0'};			    // データセグメント
-char	sync[] = { 0x55, 0x2d, 0x16 };	// 同期データ 3bytes
-char	last[] = { 0x55, 0x55, 0x55 };	// last flame
-char	scbl[] = { 0x70, 0x4f, 0x93 };	// scranbleパターン
+char	syncflm[] 	= { 0x55, 0x2d, 0x16 };	// 同期データ 3bytes
+char	lastflm[]	= { 0x55, 0x55, 0x55 };	// last flame
+char	scblflm[]	= { 0x70, 0x4f, 0x93 };	// scranbleパターン
 
 /* その他 */
 FILE    *fp;
-char	*LOGFILE = "/var/log/lastheard.log";
+char	LOGFILE[] = "/var/log/lastheard.log";
 char	logline[N] = {'\0'};
 char	line[32] = {'\0'};
 char	c[1] = {'\0'};
@@ -81,7 +87,8 @@ int     m_counter = 0;
 /* 関数の宣言 */
 int header();
 int slowdata();
-int write();
+int writelog();
 int linecount();
 
 #endif // __RECV_H__
+
