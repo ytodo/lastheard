92de5d5415e1d423cbbbadcabb51e67b766a50c7
ytodo <je3hcz@gmail.com>
2024-12-28 10:31:43 +0900

    Update get_html.py (Rpi / Alma 兼用)
    個人的に変更した方は、修正してください。

--- html/get_html.py
+++ html/get_html.py
@@ -4,18 +4,45 @@ import time
 # 指定アドレス・指定ポートからhtmlを取得する関数
 def get_html(url):
 
-	try:
-		response = requests.get(url)
-		response.raise_for_status()  # HTTPステータスコードが200番台以外の場合に例外を発生させる
-		print(f"{response.text}\n")
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # HTTPステータスコードが200番台以外の場合に例外を発生させる
+        print(f"{response.text}\n")
 
-	except requests.exceptions.RequestException as e:
-		print(f"Error: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
 
+# OSの種類を取
+def get_os_id():
+
+    # OS変数の初期化
+    os_id = None
+
+    # OSの種類を取得してos_idに代入する
+    try:
+        with open('/etc/os-release', 'r') as file:
+            for line in file:
+                if line.startswith('ID='):
+                    os_id = line.strip().split('=')[1].strip('"')
+                    break
+
+    # OS種類が取得できない時
+    except FileNotFoundError:
+        os_id = "Unknown"
+
+    return os_id
 
 if __name__ == "__main__":
-	addr = "127.0.0.1"
-	port = "20202"
-	url  = "http://" + addr + ":" + port
-	get_html(url)
 
+    # OSを取得
+    os_id = get_os_id()
+
+    # OS特有のポートを代入
+    if (os_id == "debian"):
+        port = '20202'
+    else:
+        port = '8081'
+
+    addr = "127.0.0.1"
+    url  = "http://" + addr + ":" + port
+    get_html(url)
